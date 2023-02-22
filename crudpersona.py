b34d3da15493e8c4d18f4a7f71a5dfca2230c66b
Daniel Rojas <darojas061@misena.edu.co>
2023-02-21 19:39:56 -0500

    creacion de la funcion consultar persona

--- crudpersona.py
+++ crudpersona.py
@@ -1,3 +1,30 @@
 from openpyxl import load_workbook
 from datetime import datetime
 rut=r'BaseCrudColabPersona.xlsx'
+
+def leer(ruta:str, extraer:str):
+  archivoExcel=load_workbook(ruta)
+
+  hojaDatos=archivoExcel['persona']
+  hojaDatos=hojaDatos['A2':'F'+str(hojaDatos.max_row)]
+
+  info={}
+
+  for i in hojaDatos:
+    if isinstance(i[0].value,int):
+      info.setdefault(i[0].value,{'nombre':i[1].value, 'edad':i[2].value,'telefono':i[3].value,'correo':i[4].value,'nacimiento':i[5].value})
+
+  if not(extraer=='todo'):
+    info=filtrar(info,extraer)
+  for i in info:
+    print('********** Tarea ***********')
+    print('id:'+str(i)+'\n'+'nombre: '+str(info[i]['nombre'])+'\n'+'edad: '+str(info[i]['edad'])+'\n'+'telefono: '+str(info[i]['telefono'])+'\n'+'correo: '+str(info[i]['correo'])+'\n'+'nacimiento: '+str(info[i]['nacimiento']))
+    print()
+  return info
+
+def filtrar(info:dict, filtro:str):
+  aux={}
+  for i in info:
+    if info[i]['edad']==filtro:
+      aux.setdefault(i,info[i])
+  return aux
